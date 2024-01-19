3defcacb8534a08c4cce3d41e0992d8523d2ca07
Mike Kittridge <mgkittridge@gmail.com>
2024-01-19 19:01:40 +1300

    updated

--- web_app/app/main.py
+++ web_app/app/main.py
@@ -28,217 +28,230 @@ app_base_path = pathlib.Path('/assets')
 
 page_path_names = {v['path']: v['description'] for k, v in dash.page_registry.items()}
 
-# def create_nav_link(label, href):
-#     return dcc.Link(
-#         dmc.Group(
-#             [
-#                 # dmc.ThemeIcon(
-#                 #     DashIconify(icon=icon, width=18),
-#                 #     size=30,
-#                 #     radius=30,
-#                 #     variant="light",
-#                 # ),
-#                 dmc.Text(label, size="sm", color="grey"),
-#             ]
-#         ),
-#         href=href,
-#         style={"textDecoration": "none"},
-#     )
+def create_nav_link(label, href):
+    return dcc.Link(
+        dmc.Group(
+            [
+                # dmc.ThemeIcon(
+                #     DashIconify(icon=icon, width=18),
+                #     size=30,
+                #     radius=30,
+                #     variant="light",
+                # ),
+                dmc.Text(label, size="sm", color="grey"),
+            ]
+        ),
+        href=href,
+        style={"textDecoration": "none"},
+    )
 
 
-# def create_sidebar_children(pages):
-#     """
+def create_sidebar_children(pages):
+    """
 
-#     """
-#     list1 = [
-#         dmc.Group(
-#         # direction="column",
-#         children=[
-#             create_nav_link(
-#                 label=dmc.Text("Home", style={'font-size': 16}),
-#                 href="/",
-#                 ),
-#             ],
-#             ),
-#         dmc.Divider(
-#             label=dmc.Text("User Guides", style={'font-size': 16}), style={"marginBottom": 20, "marginTop": 20}
-#             ),
-#         dmc.Divider(
-#             label=dmc.Text("Rivers", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.rivers_wq"]['title'], href=pages["pages.rivers_wq"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.rivers_wq_sites"]['title'], href=pages["pages.rivers_wq_sites"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.rivers_eco"]['title'], href=pages["pages.rivers_eco"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.rivers_eco_sites"]['title'], href=pages["pages.rivers_eco_sites"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.rivers_hfl"]['title'], href=pages["pages.rivers_hfl"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         # dmc.Group(
-#         #     children=[
-#         #         create_nav_link(
-#         #             label=pages["pages.rivers_hfl_sites"]['title'], href=pages["pages.rivers_hfl_sites"]["path"]
-#         #             )
-#         #         ], style={"marginBottom": 10}
-#         #     ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.land_cover"]['title'], href=pages["pages.land_cover"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Divider(
-#             label=dmc.Text("Lakes", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.lakes_wq"]['title'], href=pages["pages.lakes_wq"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.lakes_wq_sites"]['title'], href=pages["pages.lakes_wq_sites"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         dmc.Divider(
-#             label=dmc.Text("Groundwater", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
-#             ),
-#         dmc.Group(
-#             children=[
-#                 create_nav_link(
-#                     label=pages["pages.gw_wq"]['title'], href=pages["pages.gw_wq"]["path"]
-#                     )
-#                 ], style={"marginBottom": 10}
-#             ),
-#         ]
+    """
+    list1 = [
+        dmc.Group(
+        # direction="column",
+        children=[
+            create_nav_link(
+                label=dmc.Text("Home", style={'font-size': 16}),
+                href="/",
+                ),
+            ],
+            ),
+        dmc.Divider(
+            label=dmc.Text("User Guides", style={'font-size': 16}), style={"marginBottom": 20, "marginTop": 20}
+            ),
+        dmc.Divider(
+            label=dmc.Text("Rivers", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_wq"]['title'], href=pages["pages.rivers_wq"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_wq_sites"]['title'], href=pages["pages.rivers_wq_sites"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_wq_sites_improve"]['title'], href=pages["pages.rivers_wq_sites_improve"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_eco"]['title'], href=pages["pages.rivers_eco"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_eco_sites"]['title'], href=pages["pages.rivers_eco_sites"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.rivers_hfl"]['title'], href=pages["pages.rivers_hfl"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        # dmc.Group(
+        #     children=[
+        #         create_nav_link(
+        #             label=pages["pages.rivers_hfl_sites"]['title'], href=pages["pages.rivers_hfl_sites"]["path"]
+        #             )
+        #         ], style={"marginBottom": 10}
+        #     ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.land_cover"]['title'], href=pages["pages.land_cover"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Divider(
+            label=dmc.Text("Lakes", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.lakes_wq"]['title'], href=pages["pages.lakes_wq"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.lakes_wq_sites"]['title'], href=pages["pages.lakes_wq_sites"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.lakes_wq_sites_improve"]['title'], href=pages["pages.lakes_wq_sites_improve"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        dmc.Divider(
+            label=dmc.Text("Groundwater", style={'font-size': 16}), style={"marginBottom": 10, "marginTop": 20}
+            ),
+        dmc.Group(
+            children=[
+                create_nav_link(
+                    label=pages["pages.gw_wq"]['title'], href=pages["pages.gw_wq"]["path"]
+                    )
+                ], style={"marginBottom": 10}
+            ),
+        ]
 
-#     return list1
+    return list1
 
 
-# sidebar = dmc.Navbar(
-#     fixed=True,
-#     width={"base": 185},
-#     position={"top": 80},
-#     px=10,
-#     children=[
-#         dmc.ScrollArea(
-#             offsetScrollbars=True,
-#             type="scroll",
-#             children=create_sidebar_children(dash.page_registry)
-#             )
-#         ]
-#     )
+sidebar = dmc.Navbar(
+    fixed=True,
+    width={"base": 185},
+    position={"top": 80},
+    px=10,
+    children=[
+        dmc.ScrollArea(
+            offsetScrollbars=True,
+            type="scroll",
+            children=create_sidebar_children(dash.page_registry)
+            )
+        ]
+    )
 
 app.layout = html.Div(
     [
-      # dmc.Header(
-      #    height=60,
-      #    fixed=True,
-      #    px=25,
-      #    children=[
-      #        dmc.Grid(
-      #            # gutter='xl',
-      #            style={"height": 60},
-      #            children=[
-      #                dmc.Col(
-      #                    dmc.Anchor(
-      #                        'Mitigation Effectiveness Monitoring Design',
-      #                        size="xl",
-      #                        href="/",
-      #                        underline=False,
-      #                        # align='center',
-      #                        # style={'vertical-align': 'middle'}
+       dmc.Header(
+          height=60,
+          fixed=True,
+          px=25,
+          children=[
+              dmc.Grid(
+                  # gutter='xl',
+                  style={"height": 60},
+                  children=[
+                      dmc.Col(
+                          dmc.Anchor(
+                              'Mitigation Effectiveness Monitoring Design',
+                              size="xl",
+                              href="/",
+                              underline=False,
+                              # align='center',
+                              # style={'vertical-align': 'middle'}
 
-      #                    ),
-      #                    span=6,
-      #                    style={'padding': '20px 0'}
-      #                    # style={'vertical-align': 'middle'}
-      #                    ),
-      #                dmc.Col(
-      #                    dmc.Text(
-      #                        '',
-      #                        id='title',
-      #                        size=28,
-      #                        ),
-      #                    span=3,
-      #                    style={'padding': '20px 0'}
-      #                    ),
-      #                dmc.Col(
-      #                    dmc.Anchor(
-      #                        dmc.Image(
-      #                            src=str(app_base_path.joinpath('our-land-and-water-logo.svg')),
-      #                            fit='cover',
-      #                            width='90%'
-      #                            ),
-      #                        href='https://ourlandandwater.nz/'
-      #                        ),
-      #                    span=3,
-      #                    offset=0
-      #                    ),
-      #                ]
-      #            )
-      #        ]
-      #    ),
+                          ),
+                          span=5,
+                          style={'padding': '20px 0'}
+                          # style={'vertical-align': 'middle'}
+                          ),
+                      dmc.Col(
+                          dmc.Text(
+                              '',
+                              id='title',
+                              size=28,
+                              ),
+                          span=4,
+                          style={'padding': '20px 0'}
+                          ),
+                      dmc.Col(
+                          dmc.Anchor(
+                              dmc.Image(
+                                  src=str(app_base_path.joinpath('our-land-and-water-logo.svg')),
+                                  fit='cover',
+                                  width='90%'
+                                  ),
+                              href='https://ourlandandwater.nz/'
+                              ),
+                          span=3,
+                          offset=0
+                          ),
+                      ]
+                  )
+              ]
+          ),
 
-      #    sidebar,
+          sidebar,
         dmc.Container(
             dash.page_container,
             size="xl",
             # pt=20,
-            style={"margin-top": 20, 'margin-left': 0, 'margin-right': 0, 'margin-bottom': 20},
-            # style={"margin-top": 80, 'margin-left': 200, 'margin-right': 20},
+            # style={"margin-top": 20, 'margin-left': 0, 'margin-right': 0, 'margin-bottom': 20},
+            style={"margin-top": 80, 'margin-left': 200, 'margin-right': 20},
         ),
     ],
 )
 
-# @callback(
-#     Output('title', 'children'),
-#     Input('_pages_location', 'pathname'),
-#     prevent_initial_call=True
-#     )
-# def updated_title(path):
-#     # print(path)
-#     if isinstance(path, str):
-#         title = page_path_names[path]
-#         if title == 'Home':
-#             # title = 'Mitigation Effectiveness Monitoring Design'
-#             title = ''
+@callback(
+    Output('title', 'children'),
+    Input('_pages_location', 'pathname'),
+    prevent_initial_call=True
+    )
+def updated_title(path):
+    # print(path)
+    if isinstance(path, str):
+        title = page_path_names[path]
+        if title == 'Home':
+            title = ''
 
-#         return title
-#     else:
-#         raise dash.exceptions.PreventUpdate
+        return title
+    else:
+        raise dash.exceptions.PreventUpdate
 
 
 if __name__ == '__main__':
